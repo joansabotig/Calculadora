ce2822ec2e17b8dc1ffe6c312d7f4dddbc18bd8b
Joan <joansabotig96@gmail.com>
2018-08-22 20:35:19 -0300

    calculadora 1

---
+++ main.js
@@ -0,0 +1,46 @@
+var  valor1 ='';
+var valor2 = '';
+var ope2 = false;
+var operador = '';
+var total = '';
+function ValorBoton( valor) 
+{
+    if(ope2)
+    {
+        valor2 += valor;
+        console.log(valor2);
+    }
+    else
+    {
+        valor1 += valor;
+        console.log(valor1);
+    }
+    
+}
+function recibirOperador(operador1)
+{
+    ope2 = true;
+    operador = operador1;
+    
+}
+function calcular()
+{
+    ope2 = false;
+    var a = Number(valor1);
+    var b = Number(valor2)
+    switch(operador)
+    {
+        case '+': total = a + b;
+        break;
+        case '-': total = valor1 - valor2;
+        break;
+        case '*': total = valor1 * valor2;
+        break;
+        case '/': total = valor1 / valor2;
+        break;
+    }
+    console.log(total)
+    valor1=total;
+    valo2 =null;
+}
+
